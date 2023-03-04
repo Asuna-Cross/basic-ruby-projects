aa7c95f24c67f858c00222d39c6dda07a185d328
asuna-cross <littlecrystaldragon@gmail.com>
2023-03-04 16:17:17 +0000

    Substrings exercise complete

--- sub-strings/substrings.rb
+++ sub-strings/substrings.rb
@@ -1,1 +1,8 @@
 # A code to see how many times an item was found in an array
+
+def substrings(search_term, arr)
+    puts "#{search_term} : #{arr.count(search_term)}"
+end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+substrings("below", dictionary)
